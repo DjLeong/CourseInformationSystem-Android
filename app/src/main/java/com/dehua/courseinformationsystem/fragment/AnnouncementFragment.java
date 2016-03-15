a4defa4bdf6838ffbc3a32cf45b0f6536dec5485
leedehua <leedehua93@gmail.com>
2016-03-15 17:08:37 +0800

    Update JPush SDK configure.

--- app/src/main/java/com/dehua/courseinformationsystem/fragment/AnnouncementFragment.java
+++ app/src/main/java/com/dehua/courseinformationsystem/fragment/AnnouncementFragment.java
@@ -131,7 +131,7 @@ public class AnnouncementFragment extends Fragment {
 
     private void getJSONVolley() {
         final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.getInstance());
-        String JSONUrl = "http://172.16.112.131/CourseInformationSystem-Server/getJSON.html";
+        String JSONUrl = "http://172.16.112.131/CourseInformationSystem-Server/GetJSON";
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, JSONUrl, null,
                 new Response.Listener<JSONArray>() {
                     @Override
